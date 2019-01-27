50d2e3e4610d9d8d571430f83094ee9522634f60
Leonid Kapitonov <feacur@gmail.com>
2019-01-27 15:19:08 +0300

    sync premake with CustomEngineStudy's one

--- premake5.lua
+++ premake5.lua
@@ -1,107 +1,118 @@
 workspace "GameEngineStudy"
-    architecture "x64"
-    startproject "Sandbox"
-    warnings "Extra"
-    flags { "FatalWarnings", "NoMinimalRebuild" }
-    floatingpoint "Fast"
-    floatingpointexceptions "off"
+	toolset ("msc")
+	architecture "x64"
+	startproject "Sandbox"
+	warnings "Extra"
+	flags { "FatalWarnings", "NoMinimalRebuild" }
+	floatingpoint "Fast"
+	floatingpointexceptions "off"
 
-    configurations {
-        "Debug",
-        "Development",
-        "Shipping"
-    }
+	configurations {
+		"Debug",
+		"Development",
+		"Shipping"
+	}
 
-    filter "toolset:msc*"
-        disablewarnings {
-            "4201", -- nameless struct/union (suppress)
-            "4189", -- local variable is initialized but not referenced (suppress)
-            "4100", -- unreferenced formal parameter (suppress)
-        }
+	filter "toolset:msc*"
+		disablewarnings {
+			"4201", -- nameless struct/union (suppress)
+			"4189", -- local variable is initialized but not referenced (suppress)
+			"4100", -- unreferenced formal parameter (suppress)
+		}
+		-- buildoptions {
+		-- 	"-Bt+",
+		-- 	"-d2cgsummary",
+		-- 	"-d1reportTime",
+		-- }
+		-- linkoptions {
+		-- 	"-time+",
+		-- }
 
-    filter "toolset:gcc*"
-        disablewarnings { "unused-variable", "unused-parameter", "missing-field-initializers" }
+	filter "toolset:gcc*"
+		disablewarnings { "unused-variable", "unused-parameter", "missing-field-initializers" }
 
-    filter "configurations:Debug"
-        defines "DEBUG"
-        symbols "On"
-        optimize "Off"
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+		optimize "Off"
 
-    filter "configurations:Development"
-        defines "DEVELOPMENT"
-        symbols "On"
-        optimize "On"
+	filter "configurations:Development"
+		defines "DEVELOPMENT"
+		symbols "On"
+		optimize "On"
 
-    filter "configurations:Shipping"
-        defines "SHIPPING"
-        symbols "Off"
-        optimize "On"
+	filter "configurations:Shipping"
+		defines "SHIPPING"
+		symbols "Off"
+		optimize "On"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 enginename = "GameEngineStudy"
 
 project "GameEngineStudy"
-    location "GameEngineStudy"
-    kind "SharedLib"
-    language "C++"
+	location "GameEngineStudy"
+	kind "SharedLib"
+	language "C++"
+	characterset ("Unicode")
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    pchheader "ges_pch.h"
-    pchsource "GameEngineStudy/src/ges_pch.cpp"
+	pchheader "ges_pch.h"
+	pchsource "GameEngineStudy/src/ges_pch.cpp"
 
-    files {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs {
-        "%{prj.name}/src",
-        "%{prj.name}/vendor/spdlog/include",
-    }
+	includedirs {
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
 
-        defines {
-            "GES_PLATFORM_WINDOWS",
-            "GES_BUILD_DLL",
-        }
+		defines {
+			"GES_PLATFORM_WINDOWS",
+			"GES_BUILD_DLL",
+		}
 
-        postbuildcommands {
-            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-        }
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
 
 project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	characterset ("Unicode")
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-    }
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
 
-    includedirs {
-        (enginename .. "/src"),
-        (enginename .. "/vendor/spdlog/include"),
-    }
+	includedirs {
+		(enginename .. "/src"),
+		(enginename .. "/vendor/spdlog/include"),
+	}
 
-    links {
-        "GameEngineStudy",
-    }
+	links {
+		"GameEngineStudy",
+	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
 
-        defines {
-            "GES_PLATFORM_WINDOWS",
-        }
+		defines {
+			"GES_PLATFORM_WINDOWS",
+		}
