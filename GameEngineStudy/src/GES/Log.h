6c50436250107186b3a7dfc1f149172d3211721e
Leonid Kapitonov <feacur@gmail.com>
2019-06-30 21:01:57 +0300

    fix logging macro name and called method

--- GameEngineStudy/src/GES/Log.h
+++ GameEngineStudy/src/GES/Log.h
@@ -24,15 +24,15 @@ namespace GES {
 }
 
 // core logger
-#define GES_CORE_TRACE(...) GES::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define GES_CORE_INFO(...)  GES::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define GES_CORE_WARN(...)  GES::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define GES_CORE_ERROR(...) GES::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define GES_CORE_FATAL(...) GES::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define GES_CORE_TRACE(...)    GES::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define GES_CORE_INFO(...)     GES::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define GES_CORE_WARN(...)     GES::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define GES_CORE_ERROR(...)    GES::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define GES_CORE_CRITICAL(...) GES::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // client logger
-#define GES_TRACE(...) GES::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define GES_INFO(...)  GES::Log::GetClientLogger()->info(__VA_ARGS__)
-#define GES_WARN(...)  GES::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define GES_ERROR(...) GES::Log::GetClientLogger()->error(__VA_ARGS__)
-#define GES_FATAL(...) GES::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define GES_TRACE(...)    GES::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define GES_INFO(...)     GES::Log::GetClientLogger()->info(__VA_ARGS__)
+#define GES_WARN(...)     GES::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define GES_ERROR(...)    GES::Log::GetClientLogger()->error(__VA_ARGS__)
+#define GES_CRITICAL(...) GES::Log::GetClientLogger()->critical(__VA_ARGS__)
