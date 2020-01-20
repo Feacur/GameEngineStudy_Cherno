9dfaa22473bb8b521d9d93c54d9f5aeed8b625c9
feacur <feacur@gmail.com>
2020-01-20 13:25:55 +0300

    copy compiler settings from the custom engine project

--- project_environment_msvc.bat
+++ project_environment_msvc.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rem https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line?view=vs-2019
+
 chcp 1252
 rem code page 1250: Windows Central Europe
 rem code page 1251: Windows Cyrillic
@@ -9,16 +11,29 @@ set VSLANG=1033
 rem msbuild locale 1033: English
 
 rem read batch file arguments
-set architecture=%1
-if [%architecture%] == [] (
-	set architecture="x64"
+set architecture_compiler=%1
+set architecture_target=%2
+
+if [%architecture_compiler%] == [] (
+	echo provide architecture_compiler [x64, x86]
+	exit /b 0
+)
+
+if [%architecture_target%] == [] (
+	echo provide architecture_target [x64, x86, arm, arm64]
+	exit /b 0
 )
 
 rem clean batch file arguments
-set architecture=%architecture:"=%
+set architecture_compiler=%architecture_compiler:"=%
+set architecture_target=%architecture_target:"=%
 
 rem init
 rem pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
 pushd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
-call "vcvarsall.bat" %architecture%
+if %architecture_compiler% == %architecture_target% (
+	call "vcvarsall.bat" %architecture_compiler%
+) else (
+	call "vcvarsall.bat" %architecture_compiler%_%architecture_target%
+)
 popd
--- solution_build.bat
+++ solution_build.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rem https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line?view=vs-2019
+
 chcp 1252
 rem code page 1250: Windows Central Europe
 rem code page 1251: Windows Cyrillic
@@ -10,11 +12,22 @@ rem msbuild locale 1033: English
 
 rem read batch file arguments
 set solution=%1
-set architecture=%2
-set configuration=%3
+set architecture_compiler=%2
+set architecture_target=%3
+set configuration=%4
 
-if [%architecture%] == [] (
-	echo provide architecture [x64, x86]
+if [%solution%] == [] (
+	echo provide a solution
+	exit /b 0
+)
+
+if [%architecture_compiler%] == [] (
+	echo provide architecture_compiler [x64, x86]
+	exit /b 0
+)
+
+if [%architecture_target%] == [] (
+	echo provide architecture_target [x64, x86, arm, arm64]
 	exit /b 0
 )
 
@@ -25,12 +38,17 @@ if [%configuration%] == [] (
 
 rem clean batch file arguments
 set solution=%solution:"=%
-set architecture=%architecture:"=%
+set architecture_compiler=%architecture_compiler:"=%
+set architecture_target=%architecture_target:"=%
 set configuration=%configuration:"=%
 
 rem pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
 pushd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
-call "vcvarsall.bat" %architecture%
+if %architecture_compiler% == %architecture_target% (
+	call "vcvarsall.bat" %architecture_compiler%
+) else (
+	call "vcvarsall.bat" %architecture_compiler%_%architecture_target%
+)
 popd
 
 echo ---- BUILD ---- %time%
